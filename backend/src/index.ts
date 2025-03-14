d6455bd4a572224f0b8ea70a5ed3fcca69746c25
Shaopeng Luan <luanshaopeng@outlook.com>
2025-03-14 22:39:52 +1100

    init frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,28 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  "*",
+  cors({
+    origin:
+      process.env.HONO_CORS_ORIGIN ??
+      (() => {
+        throw new Error("HONO_CORS_ORIGIN is required");
+      })(),
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-export default app
+app.get("/", (c) => {
+  return c.text("There is nothing here.");
+});
+
+app.get("/api", (c) => {
+  return c.json({ message: "Welcome to Accelrate api!" });
+});
+
+export default app;
